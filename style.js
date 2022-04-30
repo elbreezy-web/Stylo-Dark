990ce84949d9ff6a939782b86d28b86fb0c849af
elbreezy-web <abeyol629@gmail.com>
2022-05-01 00:24:17 +0200

    first commit

---
+++ style.js
@@ -0,0 +1,61 @@
+
+
+// function arrayManipulation(array, command, location, value){
+//     if (command == 'remove' && location == 'end') {
+//         let removed = array.pop();
+//         return removed;
+//     }
+//     if(command == 'remove' && location == 'beginning'){
+//         let removeFront = array.shift();
+//         return removeFront
+//     }
+//     if(command == 'add' && location == 'beginning'){
+//         let removedFourth = array.shift(value);
+//         return removedFourth
+//     }
+//     if(command == 'add' && location == 'end'){
+//         let removedFourth2 = array.push(value);
+//         return  removedFourth2
+//     }
+// }
+
+// arrayManipulation([1,2,3], "remove", "end");
+// arrayManipulation([1,2,3,], "remove", "beginning")
+// arrayManipulation([1,2,3], "add", "beginning");
+// arrayManipulation([1,2,3], "add", "end");
+
+
+// const str = "i am a boy"
+// function stringAndNumber(Astring,number){
+//     if(Astring.length <= number){
+//         console.log(Astring.toLowerCase())
+//     }
+//     else{
+//           console.log(Astring.toUpperCase()) 
+//     }
+// }
+    
+        
+    
+
+// stringAndNumber(str,20)
+
+// function addNum(x,y){
+//     let result = x + y
+//     return result;
+// }
+// addNum(5, 5)
+
+function greaterThan(a,b){
+    if( a > b){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+}
+greaterThan(5, 10)
+
+
+
+
